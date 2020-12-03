40dfb374137baa552057850ca61b0dab49e0c135
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2020-12-03 18:00:15 +0100

    Hide account control button when none is configured

--- Linhome/views/account/AccountView.swift
+++ Linhome/views/account/AccountView.swift
@@ -41,42 +41,40 @@ class AccountView: MainViewContentWithScrollableForm {
 		model.accountDesc.readCurrentAndObserve { (text) in
 			self.viewTitle.text = text
 		}
-				
 		
-		let refresh = UIRoundRectButton(container:contentView, placedBelow:form, effectKey: "secondary_color", tintColor: "color_c", textKey: "refresh_registers", topMargin: 0)
-		let changepass = UIRoundRectButton(container:contentView, placedBelow:refresh, effectKey: "secondary_color", tintColor: "color_c", textKey: "change_password", topMargin: 23)
-		let deleteaccount = UIRoundRectButton(container:contentView, placedBelow:changepass, effectKey: "secondary_color", tintColor: "color_c", textKey: "delete_account", topMargin: 23)
-		let disconnect = UIRoundRectButton(container:contentView, placedBelow:deleteaccount, effectKey: "secondary_color", tintColor: "color_c", textKey: "menu_disconnect", topMargin: 23, isLastInContainer : true)
-		
-		
-		refresh.onClick {
-			model.refreshRegisters()
+		if (model.account != nil) {
+			let refresh = UIRoundRectButton(container:contentView, placedBelow:form, effectKey: "secondary_color", tintColor: "color_c", textKey: "refresh_registers", topMargin: 0)
+			let changepass = UIRoundRectButton(container:contentView, placedBelow:refresh, effectKey: "secondary_color", tintColor: "color_c", textKey: "change_password", topMargin: 23)
+			let deleteaccount = UIRoundRectButton(container:contentView, placedBelow:changepass, effectKey: "secondary_color", tintColor: "color_c", textKey: "delete_account", topMargin: 23)
+			let disconnect = UIRoundRectButton(container:contentView, placedBelow:deleteaccount, effectKey: "secondary_color", tintColor: "color_c", textKey: "menu_disconnect", topMargin: 23, isLastInContainer : true)
+			refresh.onClick {
+				model.refreshRegisters()
+			}
+			
+			changepass.onClick {
+				self.gotoFreeSip()
+			}
+			
+			deleteaccount.onClick {
+				self.gotoFreeSip()
+			}
+			
+			disconnect.onClick {
+				DialogUtil.confirm(titleTextKey: "menu_disconnect", messageTextKey: "disconnect_confirm_message", confirmAction: {
+					Account.it.disconnect()
+					NavigationManager.it.navigateUp()
+				})
+			}
 		}
-		
-		changepass.onClick {
-			self.gotoFreeSip()
-		}
-		
-		deleteaccount.onClick {
-			self.gotoFreeSip()
-		}
-		
-		disconnect.onClick {
-			DialogUtil.confirm(titleTextKey: "menu_disconnect", messageTextKey: "disconnect_confirm_message", confirmAction: {
-				Account.it.disconnect()
-				NavigationManager.it.navigateUp()
-			})
-		}
-		
 	}
 	
 	
 	func gotoFreeSip() {
-		  DialogUtil.confirm(titleTextKey: "account_manage_on_freesip_title", messageTextKey: "account_manage_on_freesip_message", confirmAction: {
+		DialogUtil.confirm(titleTextKey: "account_manage_on_freesip_title", messageTextKey: "account_manage_on_freesip_message", confirmAction: {
 			if let url = URL(string: Config.get().getString(section: "assistant", key: "freesip_url", defaultString: "https://subscribe.linhome.org/login")) {
-				   UIApplication.shared.open(url)
-			   }
-		   })
-	   }
+				UIApplication.shared.open(url)
+			}
+		})
+	}
 	
 }
