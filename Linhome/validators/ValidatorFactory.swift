01b620107e04c1d85f46f36cfcaa5d6725930e24
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2022-03-31 16:37:27 +0200

    Strenghten device address field

--- Linhome/validators/ValidatorFactory.swift
+++ Linhome/validators/ValidatorFactory.swift
@@ -24,6 +24,6 @@ class ValidatorFactory { // todo move into shared
         static let uriValidator = NonEmptyUrlFormatValidator()
         static let hostnameEmptyOrValidValidator = RegExpFormatValidator("^[a-zA-Z0-9.]*$", "invalid_host_name")
        	static let numberEmptyOrValidValidator = RegExpFormatValidator("^[0-9]*$", "invalid_number")
-        static let sipUri = NonEmptyWithRegExpFormatValidator("^([^@]+)(?:@(.+))?$", "invalid_sip_uri")
+        static let sipUri = NonEmptyWithRegExpFormatValidator("^(sip|sips):(.+)@(.+)$", "invalid_sip_uri")
         static let actionCode = RegExpFormatValidator("^[0-9#*]+$", "invalid_action_code")
 }
